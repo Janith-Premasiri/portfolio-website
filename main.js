50937a3fb2400af1239c42af66093cedefabfbd7
janithkanchana <janithkanchana999@gmail.com>
2023-12-07 00:35:00 +0530

    home
    css and js

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text", {
+    strings:["Undergraduate" , "Front-end Developer" , "Seeker"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    Loop: true
+
+}); 
